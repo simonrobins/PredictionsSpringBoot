9c1d662dabf4d81ffef522f77f9a4c34f0b530d4
Simon Robins <simonrobins@mac.com>
2023-03-29 13:48:34 +0100

    Added method to calculate user's scores

--- src/main/java/uk/simonrobins/PredictionsSpringBoot/repository/PredictionRepository.java
+++ src/main/java/uk/simonrobins/PredictionsSpringBoot/repository/PredictionRepository.java
@@ -13,9 +13,18 @@ public interface PredictionRepository extends CrudRepository<Prediction, Long>
 {
     List<Prediction> findAllByUserId(Long userId, Sort sort);
 
+    @Query(value="""
+        SELECT p
+        FROM Prediction p INNER JOIN p.fixture
+        WHERE (p.user.id = ?1)
+        AND   (p.fixture.round = ?2)
+        """)
+    List<Prediction> findAllByUserIdAndRound(Long userId, Integer round);
     @Query(value="SELECT new uk.simonrobins.PredictionsSpringBoot.entity.UserAndScore(p.user, count(*) score) " +
                 "FROM Prediction p INNER JOIN p.fixture f " +
-                "WHERE p.result = f.result " +
+                "WHERE (p.result = uk.simonrobins.PredictionsSpringBoot.entity.Result.HOME AND f.homeGoals > f.awayGoals) " + 
+                "OR    (p.result = uk.simonrobins.PredictionsSpringBoot.entity.Result.AWAY AND f.homeGoals < f.awayGoals) " + 
+                "OR    (p.result = uk.simonrobins.PredictionsSpringBoot.entity.Result.DRAW AND f.homeGoals = f.awayGoals) " +
                 "GROUP BY user " +
                 "ORDER BY score DESC, p.user.lastName, p.user.firstName")
     List<UserAndScore> predictionScores();
