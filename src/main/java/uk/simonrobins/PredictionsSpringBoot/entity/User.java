2fcd9336186c73ea2b5cdde703bd2c5192067179
Simon Robins <simonrobins@mac.com>
2023-03-29 13:42:41 +0100

    Add cascade as predictions should be deleted when user is deleted

--- src/main/java/uk/simonrobins/PredictionsSpringBoot/entity/User.java
+++ src/main/java/uk/simonrobins/PredictionsSpringBoot/entity/User.java
@@ -1,5 +1,6 @@
 package uk.simonrobins.PredictionsSpringBoot.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -30,7 +31,7 @@ public class User
 	@Column(nullable = false)
 	private String password;
 
-	@OneToMany(mappedBy = "user")
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 	Set<Prediction> predictions;
 
 	public User()
