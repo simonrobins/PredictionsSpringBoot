9d9ba08aeb667d7c04058efe0d79b352fda18430
Simon Robins <simonrobins@mac.com>
2023-03-29 20:31:56 +0100

    Add additional controllers

---
+++ src/main/java/uk/simonrobins/PredictionsSpringBoot/controller/web/LeagueWebController.java
@@ -0,0 +1,29 @@
+package uk.simonrobins.PredictionsSpringBoot.controller.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import uk.simonrobins.PredictionsSpringBoot.entity.TeamData;
+import uk.simonrobins.PredictionsSpringBoot.service.FixtureService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/league")
+public class LeagueWebController
+{
+    @Autowired
+    private FixtureService fixtureService;
+
+    @GetMapping
+    public String index(Model model)
+    {
+        List<TeamData> results = fixtureService.resultsTable();
+        model.addAttribute("results", results);
+
+        return "league/index";
+    }
+}
---
+++ src/main/java/uk/simonrobins/PredictionsSpringBoot/controller/web/UpcomingWebController.java
@@ -0,0 +1,42 @@
+package uk.simonrobins.PredictionsSpringBoot.controller.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import uk.simonrobins.PredictionsSpringBoot.entity.Fixture;
+import uk.simonrobins.PredictionsSpringBoot.entity.Team;
+import uk.simonrobins.PredictionsSpringBoot.service.FixtureService;
+import uk.simonrobins.PredictionsSpringBoot.service.TeamService;
+
+import java.util.Set;
+
+@Controller
+@RequestMapping("/upcoming")
+public class UpcomingWebController {
+    @Autowired
+    private FixtureService fixtureService;
+    @Autowired
+    private TeamService teamService;
+
+    @GetMapping
+    public String index(Model model,
+            @RequestParam(required = false) Integer round,
+            @RequestParam(required = false) Long teamId) {
+
+        Set<Fixture> fixtures = fixtureService.findUpcoming(round, teamId);
+        Set<Integer> rounds = fixtureService.findUpcomingRounds();
+        Set<Team> teams = teamService.findAll();
+
+        model.addAttribute("rounds", rounds);
+        model.addAttribute("teams", teams);
+        model.addAttribute("round", round);
+        model.addAttribute("teamId", teamId);
+        model.addAttribute("fixtures", fixtures);
+
+        return "upcoming/index";
+    }
+}
