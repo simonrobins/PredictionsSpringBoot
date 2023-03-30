3b8834b61274bbf4a1bd2e30abde0486dda37e6d
Simon Robins <simonrobins@mac.com>
2023-03-30 23:29:13 +0100

    Renamed

--- src/main/java/uk/simonrobins/PredictionsSpringBoot/controller/rest/XmlHttpRequestController.java
+++ src/main/java/uk/simonrobins/PredictionsSpringBoot/controller/rest/XmlHttpRequestController.java
@@ -14,20 +14,32 @@ import uk.simonrobins.PredictionsSpringBoot.service.FixtureService;
 import uk.simonrobins.PredictionsSpringBoot.service.PredictionService;
 
 @RestController
-@RequestMapping("api/")
-public class ResultController {
+@RequestMapping("/api")
+public class XmlHttpRequestController {
     @Autowired
     private PredictionService predictionService;
     @Autowired
     private FixtureService fixtureService;
 
-    @GetMapping("prediction/result/{id}/{result}")
+    @GetMapping("/predictions/{id}/{result}")
     public void updatePredictionResult(@PathVariable("id") Long id, 
             @PathVariable("result") Result result) {
         predictionService.updateResult(id, result);
     }
 
-    @GetMapping("fixture/date/{id}/{date}")
+    @GetMapping("/missing/home/{id}/{goals}")
+    public void updateHomeGoals(@PathVariable("id") Long id, 
+            @PathVariable("goals") Integer goals) {
+        fixtureService.updateHomeGoals(id, goals);
+    }
+
+    @GetMapping("/missing/away/{id}/{goals}")
+    public void updateAwayGoals(@PathVariable("id") Long id, 
+            @PathVariable("goals") Integer goals) {
+        fixtureService.updateAwayGoals(id, goals);
+    }
+
+    @GetMapping("/fixture/date/{id}/{date}")
     public void updateFixtureDate(@PathVariable("id") Long id,
             @DateTimeFormat(pattern = "yyyy-MM-dd") 
             @PathVariable("date") Date date) {
