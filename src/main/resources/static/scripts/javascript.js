7ba30536bcd896bb15edbb54ac0aa228244afd7d
Simon Robins <simonrobins@mac.com>
2023-03-30 23:34:10 +0100

    Removed WebSocket functions and updated XmlHttpRequest functions

--- src/main/resources/static/scripts/javascript.js
+++ src/main/resources/static/scripts/javascript.js
@@ -1,27 +1,6 @@
-const socket = new WebSocket('ws://localhost:8080/socket');
-
-socket.onopen = function (event) {
-    // Maybe set a flag here to indicate that the socket is open.
-};
-
-function updatePredictionWS(result, id) {
-    var data = "prediction" + ":" + result.value + ":" + id;
-    socket.send(data);
-}
-
-function updateHomeGoalsWS(goals, id) {
-    var data = "home" + ":" + goals.value + ":" + id;
-    socket.send(data);
-}
-
-function updateAwayGoalsWS(goals, id) {
-    var data = "away" + ":" + goals.value + ":" + id;
-    socket.send(data);
-}
-
 // /api/home/{fixtureId}/{homeGoals}
 function updateHomeGoals(id, goals) {
-    var url = result.form.action + "/home/" + id + "/" + goals.value;
+    var url = "/api/missing/home/" + id + "/" + goals.value;
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, true);
     xhttp.send();
@@ -29,15 +8,15 @@ function updateHomeGoals(id, goals) {
 
 // /api/home/{fixtureId}/{awayGoals}
 function updateAwayGoals(id, goals) {
-    var url = result.form.action + "/away/" + id + "/" + goals.value;
+    var url = "/api/missing/away/" + id + "/" + goals.value;
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, true);
     xhttp.send();
 }
 
-// /api/prediction/{predictionID}/{result}
+// /api/predictions/{result}/{id}
 function updatePrediction(id, result) {
-    var url = result.form.action + "/result/" + id + "/" + result.value;
+    var url = "/api/predictions/" + id + "/" + result.value;
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, true);
     xhttp.send();
