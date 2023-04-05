dc1a44e1bef1f785d0f41fb5d3cdcd0123a86480
Simon Robins <simonrobins@mac.com>
2023-04-05 15:41:11 +0100

    Added object to map JSON fixtures into

---
+++ src/main/java/uk/simonrobins/PredictionsSpringBoot/entity/Fixture2.java
@@ -0,0 +1,13 @@
+package uk.simonrobins.PredictionsSpringBoot.entity;
+
+public class Fixture2 {
+    public int MatchNumber;
+    public int RoundNumber;
+    public String DateUtc;
+    public String Location;
+    public String HomeTeam;
+    public String AwayTeam;
+    public String Group;
+    public Integer HomeTeamScore;
+    public Integer AwayTeamScore;
+}
